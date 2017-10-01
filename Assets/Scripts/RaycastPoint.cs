0f25cbba124d5ce4c6635f3fa76140a15c9e95b9
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2017-10-01 17:24:16 +0100

    Raycast point script added but not fully functional
    - intended use to get the idea of using raycast transforms for enemy
    placement

---
+++ Assets/Scripts/RaycastPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastPoint : MonoBehaviour {
+    private LayerMask ground;
+    Light hit_light;
+
+    public float max_distance = 100;
+
+	// Use this for initialization
+	void Start () {
+        ground = LayerMask.GetMask("Ground");
+        //hit_light = GetComponent <Light> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, max_distance, ground))
+        {         
+            Vector3 hit_position = transform.InverseTransformPoint(hit.point);
+            Debug.Log(hit_position);
+            transform.position = hit.point;
+        }
+    }
+}
