40b96de479463f8044cd2b5fc1d1981c045a62ff
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2017-10-30 09:56:16 +0000

    HAHAH EVERYTHIGN IS STILL BROKEN HEHE

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,37 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_Movement : MonoBehaviour {
 
-    public Transform[] goals;
-    UnityEngine.AI.NavMeshAgent agent;
-    int i = 0;
+    private GameObject player;
+    public Transform[] patrolPoints; //Add in inspector
+
+    private int patrolPoint = 0;
+    private NavMeshAgent agent;
+    private Light light;
+
     void Start()
     {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        light = GetComponentInChildren<Light>();
+        player = GameObject.Find("Player");
+        agent = GetComponent<NavMeshAgent>();
+
     }
 
-    // Update is called once per frame
-    void Update ()
+    void Patrol()
     {
+        //if (patrolPoints.Length > 0)
+        //{
+        //    agent.SetDestination(patrolPoints[patrolPoint].transform.position);
+        //    if (transform.position == patrolPoints[patrolPoint].transform.position || Vector3.Distance(transform.position, patrolPoints[patrolPoint].transform.position) < 0.2f)
+        //    {
+        //        patrolPoint++;    //use distance if needed(lower precision)
+        //    }
+        //    if (patrolPoint >= patrolPoints.Length)
+        //    {
+        //        patrolPoint = 0;
+        //    }
+        //}
+    }
 
-		//if(transform.position == goals[i].position)
-  //      {
-  //          if(i < goals.Length)
-  //          {
-  //              i++;
-  //              agent.destination = goals[i].position;
+    void Attack()
+    {
+        light.color = Color.red;
+        //??? your job...
+        //Debug.Log("Whaaaaaaa");    //just to get informed
+    }
 
-  //          }
-  //          else
-  //          {
-  //              i--;
-  //              agent.destination = goals[i].position;
-  //          }
-  //      }
+ 
+
+    void Update()
+    {
+        if (!Physics.Linecast(transform.position, player.transform.position, 1))
+        { //check if we see player by linecasting,move player to another layer so the ray won't hit it. 
+            Attack();
+        }
+        else
+        {
+            light.color = Color.green;
+
+            Patrol();
+        }
 
     }
 
-    
 }
